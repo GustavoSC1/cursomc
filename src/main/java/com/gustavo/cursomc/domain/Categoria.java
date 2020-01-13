e450bebd2a7a6e954706b330654bf785f7cfcd32
Gustavo Silva Cruz <gu.cruz17@hotmail.com>
2020-01-12 22:09:47 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/gustavo/cursomc/domain/Categoria.java
+++ src/main/java/com/gustavo/cursomc/domain/Categoria.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 //Serializable diz que os objetos dessa classe poderão ser convertidos
 //em uma sequencia de bytes, e assim eles podem ser gravados em arquivos
@@ -22,6 +24,7 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
+	@JsonManagedReference
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
