a3375c00c7620d097e5fde3eed4099c458bfe63f
Gustavo Silva Cruz <gu.cruz17@hotmail.com>
2020-02-01 12:03:33 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/gustavo/cursomc/CursomcApplication.java
+++ src/main/java/com/gustavo/cursomc/CursomcApplication.java
@@ -1,5 +1,9 @@
 package com.gustavo.cursomc;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +12,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //quando estiver contido em um SpringApplication. 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@PostConstruct
+    void started() {
+      TimeZone.setDefault(TimeZone.getTimeZone("Hora oficial do Brasil"));
+    }
 		
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
