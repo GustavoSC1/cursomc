2506587c7ac124312d00790a376c9f28ffb294b0
Gustavo Silva Cruz <gu.cruz17@hotmail.com>
2020-02-07 21:10:12 -0300

    MockEmailService com Logger. Padrões Strategy e Template

---
+++ src/main/java/com/gustavo/cursomc/services/EmailService.java
@@ -0,0 +1,12 @@
+package com.gustavo.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.gustavo.cursomc.domain.Pedido;
+
+public interface EmailService {
+	
+	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+}
